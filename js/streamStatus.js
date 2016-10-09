6f8dcae2a4fd1f390462e5d5c917bf21f6924ccd
matt <matt@mattunderscore.co>
2016-10-08 19:16:57 -0700

    commit all

---
+++ js/streamStatus.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {   
+
+	var twitchName = "pandaplayshd";
+	var youtubeName = "pandaplaysmchd";
+   $.getJSON("https://api.twitch.tv/kraken/streams/" + twitchName + "?callback=?",function(streamData) {
+      console.log('Stream Data:', streamData)
+      if(streamData && streamData.stream) {
+         $.getJSON('https://api.twitch.tv/kraken/channels/' + twitchName + '?callback=?', function(data) {
+			document.getElementById('title').textContent = data.status;
+			document.getElementById('player').src = "https://player.twitch.tv/?channel=" + twitchName;
+});
+      } else {
+         document.getElementById('title').textContent = "Most Recent YouTube Video";
+		 document.getElementById('player').src = "https://www.youtube.com/embed?listType=user_uploads&list=" + youtubeName;
+      }
+   })
+})
